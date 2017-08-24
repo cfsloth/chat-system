b16004284cfd4ed7cb1eea0de8383f5019df19eb
Claudio Gonçalves <claudiofilipesilvagoncalves@gmail.com>
2017-08-24 14:16:19 +0100

    Understanding how rooting works in Go programming language

--- httpPage.go
+++ httpPage.go
@@ -3,13 +3,22 @@ package main
 import ( 
 	"fmt"
 	"net/http"
+	"html/template"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s", r.URL.Path[1:])
+	t,_ := template.ParseFiles("./template/index.html");
+	t.Execute(w,"");
+}
+
+func hello(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w,"Hello Go");
 }
 
 func main(){
 	http.HandleFunc("/",handler)
+	http.HandleFunc("/chat",hello);
 	http.ListenAndServe(":8080",nil)
 }
+
+
