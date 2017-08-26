0fda5ea69a4a9e2c8a07f0d2af537983db6f1b8d
Claudio Gonçalves <claudiofilipesilvagoncalves@gmail.com>
2017-08-26 04:49:40 +0100

    Updates on login and register user

--- controller/user.go
+++ controller/user.go
@@ -1,36 +1,50 @@
-package user
+package controller
 
 import (
 	"fmt"
 	"net/http"
+	"Chat-System/chat-system/model"
 )
 
-//Probable turn this into a login function
-func LoginUser(w http.ResponseWriter, r *http.Request){
+//Controls the page
+func LoadPageAndMethods(w http.ResponseWriter, r *http.Request){
 	if r.Method == "GET" {
 		http.ServeFile(w,r,"template/index.gtpl")
 		fmt.Println("GET /")
 	}else{
-		fmt.Println("POST /")
 		r.ParseForm()
-		if r.PostFormValue("username") == "claudio" && r.PostFormValue("password") == "admin" {
-			//Insert model method to acess database
-			fmt.Println("Login sucessFull")
-			http.Redirect(w, r, "/chatMessage", http.StatusSeeOther)
+		if r.PostFormValue("name") != "" {
+			fmt.Println("POST /registe")
+			RegisterUser(w,r)
+		}else{
+			fmt.Println("POST /login")
+			LoginUser(w,r)
 		}
 	}
 }
 
-//Incomplete implement database first
+//Does the login - incomplete
+func LoginUser(w http.ResponseWriter, r *http.Request){
+		if r.PostFormValue("email") == "claudio" && r.PostFormValue("password") == "admin" {
+			//Insert model method to acess database
+			fmt.Println("Login sucessFull")
+			http.Redirect(w, r, "/chatMessage", http.StatusSeeOther)
+		}else{
+			panic("Wrong login")
+		}
+}
+
+//Registe user - incomplete
 func RegisterUser(w http.ResponseWriter, r *http.Request){
 	var(
 		registered bool = false;
 	)
 	if !registered {
-		r.ParseForm()
-	//	string name = r.PostFormValue("name")
-	//	string password = r.PostFormValue("password")
-	//	string email = r.PostFormValue("email")
+		name := r.PostFormValue("name")
+		password := r.PostFormValue("password")
+		email := r.PostFormValue("email")
+		session := model.InitializeDB()
+		model.InsertUser(session,email,password,name)
 		//Send to database
 	}else {
 		panic("Error user already exists")
